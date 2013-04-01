aa7dc14a4e88d1a7ae119296a4bb6da1e0f96751
Brett L. Schuchert <schuchert@yahoo.com>
2013-04-01 15:18:39 -0400

    @Ignore Cucumber runners

--- src/test/java/shoe/example/cucumber/LightweightCucumberExecutorTest.java
+++ src/test/java/shoe/example/cucumber/LightweightCucumberExecutorTest.java
@@ -9,6 +9,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import shoe.example.context.SystemApplicationContext;
 import shoe.example.server.EmbeddedJetty;
 
+@Ignore
 @RunWith(Cucumber.class)
 @Cucumber.Options(format = {"pretty", "html:target/cucumber-html-report"}, features = "src/test/resources", glue = "shoe/example/features/step_definitions")
 public class LightweightCucumberExecutorTest {
